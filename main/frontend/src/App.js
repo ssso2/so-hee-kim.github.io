a5a044c4fa1a23c4861a4591a01402d4c6601d5f
sohee <01okok@naver.com>
2024-12-26 17:12:12 +0900

    기초 가이드 커밋

---
+++ main/frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header_1 from "./components/header/Header_1";
+
+function App() {
+    return (
+        <>
+            <Header_1 />
+        </>
+    );
+}
+
+export default App;
---
+++ main/frontend/src/components/header/Header_1.js
@@ -0,0 +1,15 @@
+import "../../scss/reset.css";
+import "../../scss/common.scss";
+import "../../scss/header.scss";
+
+const Header_1 = () => {
+    return (
+        <header className="header-wrap">
+            <div className="center">
+                <div className="txt">헤더입니다.</div>;
+                <div className="box">박스</div>
+            </div>
+        </header>
+    );
+};
+export default Header_1;
